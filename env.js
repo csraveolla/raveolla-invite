3261044dd0b503fa7ac2c6c7c7b32487390a8d5f
csraveolla <cs.raveolla@gmail.com>
2026-07-15 12:43:48 +0700

    cleanup: remove Cloudflare Pages files, ready for shared hosting
    - Delete functions/[[catchall]].js (Cloudflare Function)
    - Delete _routes.json (Cloudflare config)
    - env.js excluded from git (upload manually to server)
    - Root index.html is simple 404 page

--- env.js
+++
@@ -1,7 +0,0 @@
-window.__ENV = {
-  SUPABASE_URL:  'https://qyaxacjktcuyvkcrnofc.supabase.co',
-  SUPABASE_KEY:  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5YXhhY2prdGN1eXZrY3Jub2ZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3OTY3NjYsImV4cCI6MjA5ODM3Mjc2Nn0.p1-9aFY_WZ4aVGg3D8Mx_4NUdqp8RZkXil6sX2-VA70',
-  PUBLIC_DOMAIN: 'https://raveolla-nikahin.pages.dev',
-  FONNTE_API:    'https://api.fonnte.com/send',
-  DEFAULT_THEME: 'sakina'
-}
